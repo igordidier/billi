93456cacccce8d3d1d8666ee8a1b34c100ab01dd
igordidier <46652986+igordidier@users.noreply.github.com>
2020-01-13 22:48:28 +0100

    header

--- index.php
+++ index.php
@@ -19,33 +19,7 @@
   <?php include "boot.html"; ?>
 </head>
 <body>
-  <div class="header">
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <a class="navbar-brand" href="#">Billi</a>
-
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-
-        <li class="nav-item">
-          <a class="nav-link" href="index.php">Idea</a>
-        </li>
-
-      </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
-      <li class="nav-item active">
-        <?php    echo '<a class="nav-link" href="logout.php">log out</a>';
-        echo '<a class="nav-link" href="profil.php">'.  ucfirst($_SESSION["username"]) . '</a>';
-        ?>
-      </li>
-    </div>
-  </nav>
-</div>
+<?php include "header.html"; ?>
 
 
   	 <!-- notification message -->
