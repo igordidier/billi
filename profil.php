b0a420717b9ad8a4d9090ff414aa2caad6aff231
igordidier <46652986+igordidier@users.noreply.github.com>
2020-01-16 15:33:03 +0100

    pp

--- profil.php
+++ profil.php
@@ -21,43 +21,7 @@
 <body>
   <?php include "header.php"; ?>
 
-<?php
-  if (isset($_POST['save_profile'])) {
-     // for the database
-     $bio = stripslashes($_POST['bio']);
-     $profileImageName = time() . '-' . $_FILES["profileImage"]["name"];
-     // For image upload
-     $target_dir = "images/";
-     $target_file = $target_dir . basename($profileImageName);
-     // VALIDATION
-     // validate image size. Size is calculated in Bytes
-     if($_FILES['profileImage']['size'] > 200000) {
-       $msg = "Image size should not be greated than 200Kb";
-       $msg_class = "alert-danger";
-     }
-     // check if file exists
-     if(file_exists($target_file)) {
-       $msg = "File already exists";
-       $msg_class = "alert-danger";
-     }
-     // Upload image only if no errors
-     if (empty($error)) {
-       if(move_uploaded_file($_FILES["profileImage"]["tmp_name"], $target_file)) {
-         $sql = "INSERT INTO users SET profile_image='$profileImageName', bio='$bio'";
-         if(mysqli_query($conn, $sql)){
-           $msg = "Image uploaded and saved in the Database";
-           $msg_class = "alert-success";
-         } else {
-           $msg = "There was an error in the database";
-           $msg_class = "alert-danger";
-         }
-       } else {
-         $error = "There was an erro uploading the file";
-         $msg = "alert-danger";
-       }
-     }
-   }
- ?>
+
 
 
   <div class="container">
@@ -70,11 +34,13 @@
             <thead>
               <th>Image</th>
               <th>Bio</th>
+
             </thead>
             <tbody>
 
                 <tr>
                   <td> <img style="margin:auto;display:flex;" src="ppimg/<?php echo "$_SESSION[profile_image] " ?>" width="200" height="200" /> </td>
+                  <td>  <?php echo "$_SESSION[bio] " ?>   </td>
                 </tr>
 
             </tbody>
